f9736a01dc99bcc1e62bdd5911b40a9cd222d05c
kbogar <krisztianbogar@gmail.com>
2023-07-05 15:17:21 +0000

    Add logo to navbar,icons and favicon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo from "../assets/logo.png";
 
 const NavBar = () => {
     return (
         <Navbar bg="light" expand="lg" fixed="top">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand>
+                    <img src={logo} alt="logo" height="50" />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link><i class="fa-solid fa-tent"></i>Home</Nav.Link>
+                        <Nav.Link><i class="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
+                        <Nav.Link><i class="fa-solid fa-user-plus"></i>Sign up</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
