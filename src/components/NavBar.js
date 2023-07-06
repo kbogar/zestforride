25c90c3a1c35fa30e6a6885e7664c3a6226973c3
kbogar <krisztianbogar@gmail.com>
2023-07-06 14:42:22 +0000

    Add custom css to navbar and app

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from "../assets/logo.png";
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
     return (
-        <Navbar bg="light" expand="lg" fixed="top">
+        <Navbar className={styles.NavBar} expand="lg" fixed="top">
             <Container>
                 <Navbar.Brand>
                     <img src={logo} alt="logo" height="50" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
+                    <Nav className="ml-auto text-right">
                         <Nav.Link><i class="fa-solid fa-tent"></i>Home</Nav.Link>
                         <Nav.Link><i class="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
                         <Nav.Link><i class="fa-solid fa-user-plus"></i>Sign up</Nav.Link>
