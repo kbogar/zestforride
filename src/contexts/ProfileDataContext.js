fbb50546d03e88540e60819ca2abbe87527f3698
kbogar <krisztianbogar@gmail.com>
2023-07-21 08:57:14 +0000

    Create ProfilePage header to show users name, image, posts

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -2,8 +2,8 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { axiosReq } from "../api/axiosDefaults";
 import { useCurrentUser } from "./CurrentUserContext";
 
-const ProfileDataContext = createContext();
-const SetProfileDataContext = createContext();
+export const ProfileDataContext = createContext();
+export const SetProfileDataContext = createContext();
 
 export const useProfileData = () => useContext(ProfileDataContext);
 export const useSetProfileData = () => useContext(SetProfileDataContext);
@@ -23,7 +23,7 @@ export const ProfileDataProvider = ({ children }) => {
                 const { data } = await axiosReq.get(
                     '/profiles/?ordering=-followers_count'
                 );
-                setProfileData(prevState => ({
+                setProfileData((prevState) => ({
                     ...prevState,
                     popularProfiles: data,
                 }));
@@ -36,7 +36,7 @@ export const ProfileDataProvider = ({ children }) => {
 
     return (
         <ProfileDataContext.Provider value={profileData}>
-            <SetProfileDataContext.Provider value={{ setProfileData }}>
+            <SetProfileDataContext.Provider value={ setProfileData }>
                 {children}
             </SetProfileDataContext.Provider>
         </ProfileDataContext.Provider>
