cd2828a41ad8ac3e08a94088ed27b5b2a4d9f6b3
kbogar <krisztianbogar@gmail.com>
2023-07-23 12:54:26 +0000

    Add more styling to ContactConfirmation

--- src/pages/contact/ContactConfirmation.js
+++ src/pages/contact/ContactConfirmation.js
@@ -1,29 +1,26 @@
 import React from "react";
 import styles from "../../styles/ContactCreateForm.module.css";
 import appStyles from "../../App.module.css";
-import {Col, Row, Container, Image} from "react-bootstrap";
-
+import { Col, Row, Container, Image } from "react-bootstrap";
 
 const ContactConfirmation = () => {
     return (
         <Row className={styles.Row}>
 
-            <Col className="my-auto py-2 p-md-2" md={6}>
+            <Col>
                 <Container className={`${appStyles.Content} p-4 `}>
                     <h1 className={styles.Header}>Thank you</h1>
                     <p className={styles.Content}>We have received your message and will be in touch soon!</p>
+                    <Image
+                        className={`${appStyles.FillerImage}`}
+                        src={"https://res.cloudinary.com/dafz5mtkx/image/upload/v1690112999/pexels-gratisography-519_wk9i96.jpg"}
+                    />
                 </Container>
+
             </Col>
-            <Col
-                md={6}
-                className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
-            >
-                <Image
-                    className={`${appStyles.FillerImage}`}
-                    src={"https://res.cloudinary.com/dafz5mtkx/image/upload/v1690112999/pexels-gratisography-519_wk9i96.jpg"}
-                />
-            </Col>
-        </Row>
+
+
+        </Row >
     );
 };
 
